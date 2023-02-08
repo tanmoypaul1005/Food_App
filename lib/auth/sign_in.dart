a3ca96d5fad39867eefa71b6ffc7921cb5c2d15f
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-08 21:39:26 +0600

    ok

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +7,12 @@ class SingIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/background.png')),
+        ),));
   }
 }
